ef5061d9d057bb35ee286f001d9556f94feea78c
Prachi <prachik3002@gmail.com>
2024-04-10 12:09:16 +0530

    new file created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+import {SnackbarProvider} from 'notistack'
+import Home from './Components/Home'
+import Signup from './Components/Signup'
+import Login from './Components/Login'
+import Eventhandling from './Components/Eventhandling'
+import Navbar from './Components/Navbar'
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <SnackbarProvider>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Home' element={<Home />} />
+        <Route path='/Signup' element={<Signup/>} />
+        <Route path='/Login' element={<Login/>} />
+        <Route path='/EventHandling' element={<Eventhandling/>} />
+      </Routes>
+      </BrowserRouter>
+      </SnackbarProvider>
+    </div>
   )
 }
 
